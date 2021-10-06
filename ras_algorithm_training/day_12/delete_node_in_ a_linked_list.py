b874a8d3dbf6ca3b5d9214a5d18ae6fe06972ab0
Omar Omran <omaromran2003@gmail.com>
2021-10-06 18:37:25 +0400

    day 12

---
+++ ras_algorithm_training/day_12/delete_node_in_ a_linked_list.py
@@ -0,0 +1,10 @@
+class ListNode:
+   def __init__(self, x):
+       self.val = x
+       self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+	next = node.next
+        node.val = next.val
+        node.next = next.next
